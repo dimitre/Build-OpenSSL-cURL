43f802ddc5662f66889cff520f0338752fbf8a5b
Dimitre <dimitre@users.noreply.github.com>
2025-01-19 21:03:10 -0300

    ok build.sh

--- build.sh
+++ build.sh
@@ -187,7 +187,7 @@ fi
 echo
 cd openssl
 echo -e "${bold}Building OpenSSL${normal}"
-# ./openssl-build.sh -v "$OPENSSL" $engine $colorflag $catalyst $sslv3 $OSARGS
+./openssl-build.sh -v "$OPENSSL" $engine $colorflag $catalyst $sslv3 $OSARGS
 cd ..
 
 ## Nghttp2 Build
@@ -252,7 +252,7 @@ mkdir -p "$ARCHIVE/xcframework"
 # cp curl/lib/libcurl_iOS-fat.a $ARCHIVE/lib/iOS-fat/libcurl.a
 # cp curl/lib/libcurl_tvOS.a $ARCHIVE/lib/tvOS/libcurl.a
 # cp curl/lib/libcurl_tvOS-simulator.a $ARCHIVE/lib/tvOS-simulator/libcurl.a
-cp curl/lib/libcurl_Mac.a $ARCHIVE/lib/MacOS/libcurl.a
+cp curl/lib/libcurl.a $ARCHIVE/lib/MacOS/libcurl.a
 
 # cp openssl/iOS/lib/libcrypto.a $ARCHIVE/lib/iOS/libcrypto.a
 # cp openssl/iOS-simulator/lib/libcrypto.a $ARCHIVE/lib/iOS-simulator/libcrypto.a
